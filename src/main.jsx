10389907c54a545de2eae9a14504409a2fe1dfcb
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-29 20:28:01 -0700

    src/pages created

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
@@ -8,7 +8,7 @@ const root = createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App/>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
