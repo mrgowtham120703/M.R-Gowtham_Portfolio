7a07af7c5d3a0ddd44777f6a537f4bf457b8adea
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-29 20:32:24 -0700

    App.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from
+import HomePage from './pages/HomePage';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <div className='min-h-screen bg-gray-900 text-white'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/about' element={<AboutMe/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </div>
+  )
+}
